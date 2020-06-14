ad6616f35be9d05e8e1f584b1129d8c065e83522
Merojiddin <merojiddin_1998@mail.ru>
2020-06-15 00:49:05 +0800

    before pull

--- src/@config/env.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  authToken: process.env.REACT_APP_AUTH_TOKEN || '',
-  chatUrl: process.env.REACT_APP_CHAT_URL || 'http://localhost:7070',
-  backendUrl:
-    process.env.REACT_APP_BACKEND_URL || 'http://localhost:9999/graphql',
-};
